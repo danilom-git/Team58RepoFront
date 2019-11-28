24f51584410c4cfc09c67892b27abc58db0af965
djordje <Zoomb1xon>
2019-11-28 05:37:15 +0100

    bez redux-a

---
+++ src/components/clinicProfil.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import ClinicProfilNav from "./clinicProfileNav";
+import Doctors from "./doctors";
+import DoctorAddForm from "./doctorAddForm";
+
+class ClinicProfil extends Component {
+  state = { main: <div></div> };
+
+  showDoctors = () => {
+    this.setState({ main: <Doctors changeToForm={this.changeToForm} /> });
+  };
+
+  changeToForm = () => {
+    this.setState({ main: <DoctorAddForm /> });
+  };
+
+  render() {
+    return (
+      <div className="row">
+        <div className="col-sm-12">
+          <ClinicProfilNav showDoctors={this.showDoctors} />
+        </div>
+        <div className="col-sm-12">{this.state.main}</div>
+      </div>
+    );
+  }
+}
+
+export default ClinicProfil;
---
+++ src/components/doctorProfil.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import doctorNav from "./doctorNav";
+import DoctorNav from "./doctorNav";
+
+class DoctorProfil extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <DoctorNav />
+      </div>
+    );
+  }
+}
+
+export default DoctorProfil;
