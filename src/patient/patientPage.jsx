42e226e779752a7da0af43f045d9e3cdc8b1c2c0
djordje <Zoomb1xon>
2019-12-04 23:57:00 +0100

    Merge remote-tracking branch 'origin/t9.2_displaying_doctors' into t36_edit_doctors
    # Conflicts:
    #	.idea/workspace.xml

---
+++ src/patient/patientPage.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class PatientPage extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default PatientPage;
