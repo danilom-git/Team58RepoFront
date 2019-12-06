eb502af89aac92dbf7e699b48000a7c75d1939f1
djordje <Zoomb1xon>
2019-12-06 03:31:50 +0100

    checking hall and doctor

--- src/components/oneClickForm.jsx
+++ src/components/oneClickForm.jsx
@@ -13,6 +13,7 @@ class OneClickForm extends Component{
         doctors: [],
         halls: [],
         types:[],
+        type:{},
         oneClickCheckup:{},
         date:{},
         duration:{},
@@ -25,7 +26,7 @@ class OneClickForm extends Component{
         disableTime:true
     }
 
-    componentDidMount() {
+    componentDidMount() {//get doctors,halls,types
         Axios.get('http://localhost:8080/api/doctors/all').then((res) => {
             this.setState({doctors:res.data});
           //  console.log(this.state.doctors);
@@ -42,7 +43,7 @@ class OneClickForm extends Component{
         })
     }
 
-    getCheckupsOnDay = (e) => {
+    getCheckupsOnDay = (e) => {//checkups for one day
         if (e.target.value) {
             Axios.get("http://localhost:8080/api/checkups/allOnDate/" + e.target.value).then((res) => {
                 this.setState({checkupsOneDay: res.data});
@@ -57,42 +58,96 @@ class OneClickForm extends Component{
         }
     }
 
-    changePrice = (e) => {
+    changePrice = (e) => {  //SET PRICE
         console.log(e.target.value);
         this.setState({price:e.target.value});
     }
 
-    handleDoctors = (e) => {
-        this.setState({doctor:e.target.value});
+    handleDoctors = (e) => {// SET DOCTOR ID
+       this.setState({doctor:e.target.value});
+       this.checkDoctor(e.target.value);
     }
 
-    handleHalls = (e) => {
+    handleHalls = (e) => {// SET HALL ID
         this.setState({hall:e.target.value});
+        this.checkHall(e.target.value);
     }
 
-    handleTypes = (e) => {
+    handleTypes = (e) => {// SET TYPE ID s
         this.setState({type:e.target.value});
     }
 
-    checkDoctor = () => {
-        for(let d in this.state.checkupsOneDay)
+    checkDoctor = (id) => {
+
+        for(let d of this.state.checkupsOneDay)
         {
-            //if(this.state.doctor)
+            if(id === d.doctorId.toString())
+            {
+                console.log(d.doctorId);
+                let sp1 = d.startDate.split(".");
+                let dStartDate = new Date(sp1[0]);
+                let sp2 = d.endDate.split(".");
+                let dEndDate = new Date(sp2[0]);
+
+                console.log(dStartDate.getTime(),"za ",dStartDate,this.state.startDate.getTime(),"za",this.state.startDate);
+
+                if((this.state.startDate.getTime() <= dStartDate.getTime() && this.state.endDate.getTime() <= dStartDate.getTime())||(this.state.startDate.getTime() >= dEndDate.getTime() && this.state.endDate.getTime() >= dEndDate.getTime()))
+                {
+                    console.log("OK DOCTOR");
+                }else
+                {
+                    this.setState({doctor:false});
+                    console.log("NIJE OK DOCTOR");
+                    break;
+                }
+            }else
+            {
+                console.log("NIJE ID");
+            }
         }
+
     }
 
-    checkHall = () => {
+    checkHall = (id) => {
+        for(let d of this.state.checkupsOneDay)
+        {
+            if(id === d.hallId.toString())
+            {
+                console.log(d.hallId);
 
+                let sp1 = d.startDate.split(".");
+                let dStartDate = new Date(sp1[0]);
+                let sp2 = d.endDate.split(".");
+                let dEndDate = new Date(sp2[0]);
+
+                console.log(dStartDate.getTime(),this.state.startDate.getTime());
+
+                if((this.state.startDate.getTime() <= dStartDate.getTime() && this.state.endDate.getTime() <= dStartDate.getTime())||(this.state.startDate.getTime() >= dEndDate.getTime() && this.state.endDate.getTime() >= dEndDate.getTime()))
+                {
+                    console.log("OK SALA");
+                }else
+                {
+                    this.setState({hall:false});
+                    console.log("NIJE OK SALA");
+                    break;
+                }
+            }else
+            {
+                console.log("NIJE ID");
+            }
+        }
     }
 
     trigDisableTrue = () => {
         this.setState({disable:false});
-
     }
 
     changeStartTime = (e) => {
+        //(this.state.date + "T" + e.target.value + ":00.000+0000")
+        let dateString = this.state.date + "T" + e.target.value + ":00";
+        console.log(dateString);
         if(e.target.value) {
-            this.setState({startDate: (this.state.date + "T" + e.target.value + ":00.000+0000")});
+            this.setState({startDate: new Date(dateString)});
             if (this.state.endDate) {
                 this.trigDisableTrue();
             }
@@ -102,26 +157,29 @@ class OneClickForm extends Component{
     }
 
     changeEndTime = (e) => {
+        let dateString = this.state.date + "T" + e.target.value + ":00";
         if(e.target.value) {
-            this.setState({endDate: (this.state.date + "T" + e.target.value + ":00.000+0000")});
+            this.setState({endDate: new Date(dateString)});
             if (this.state.startDate)
                 this.trigDisableTrue();
         }else{
             this.setState({disable:true});
         }
     }
+
     handleSubmit = (e) => {
         e.preventDefault();
-        if((!this.state.disable) && (!this.state.disableTime) && this.state.price)
-        {
 
+        if((!this.state.disable) && (!this.state.disableTime) && this.state.price && this.state.doctor && this.state.hall)
+        {
+            console.log("state iz submita",this.state);
         }
     }
 
     render() {
-        const types = this.state.types.map(type => (<option value={type} key={type.id}>{type.name}</option>));
-        const doctors = this.state.doctors.map(doctor => (<option value={doctor} key={doctor.id}>{doctor.name + " " + doctor.lastName}</option>));
-        const halls = this.state.halls.map(hall => (<option value={hall} key={hall.id}>{hall.name}</option>));
+        const types = this.state.types.map(type => (<option value={type.id} key={type.id}>{type.name}</option>));
+        const doctors = this.state.doctors.map(doctor => (<option value={doctor.id} key={doctor.id}>{doctor.name + " " + doctor.lastName}</option>));
+        const halls = this.state.halls.map(hall => (<option value={hall.id} key={hall.id}>{hall.name}</option>));
         return(<form>
             <div className="row">
                 <div className="col-sm-2 m-1">
@@ -155,7 +213,7 @@ class OneClickForm extends Component{
                     <label>Doctor:</label>
                 </div>
                 <div col="col-sm-4">
-                    <select onChange={this.handleDoctors} disabled = {this.state.disable} className="form-control">{doctors}</select>
+                    <select  onChange={this.handleDoctors}  disabled = {this.state.disable} className="form-control"><option></option>{doctors}</select>
                 </div>
             </div>
 
@@ -164,7 +222,7 @@ class OneClickForm extends Component{
                     <label>Hall:</label>
                 </div>
                 <div col="col-sm-4">
-                    <select onChange={this.handleHalls} disabled = {this.state.disable} className="form-control">{halls}</select>
+                    <select onChange={this.handleHalls}  disabled = {this.state.disable} className="form-control"><option></option>{halls}</select>
                 </div>
             </div>
 
@@ -173,7 +231,7 @@ class OneClickForm extends Component{
                     <label>Type:</label>
                 </div>
                 <div col="col-sm-4">
-                    <select onChange={this.handleTypes} disabled = {this.state.disable} >{types}</select>
+                    <select onChange={this.handleTypes} disabled = {this.state.disable} ><option></option>{types}</select>
                 </div>
             </div>
 
