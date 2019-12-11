dc3a78a4fe43fbdad9ee2b4dc885df4ebfdfed11
djordje <Zoomb1xon>
2019-12-11 23:23:54 +0100

    show all patients

--- src/doctor/doctorProfil.jsx
+++ src/doctor/doctorProfil.jsx
@@ -1,16 +1,26 @@
 import React, { Component } from "react";
 import Sidebar from "../generic_components/sidebar";
+import Patients from "../patient/patients";
 
 class DoctorProfil extends Component {
-  state = {};
+  state = {
+      main: <div></div>
+  };
+
+  showPatients = () => {
+      this.setState({main:<Patients />});
+  }
+
   render() {
     return (
         <div className="container-fluid pt-2">
-          <div className="row">
-            <Sidebar
-              links={[{id: 1, text: 'User profile'},{id: 2, text: 'Patients'},{id: 6, text: 'Working calendar'},{id: 7, text: 'Absence request'},{id: 8, text: 'Annual leave request'}]}
-            />
-          </div>
+            <div className="row">
+                <Sidebar
+                links={[{id: 1, text: 'User profile'},{id: 2, text: 'Patients',onClick:this.showPatients},{id: 3, text: 'Working calendar'},{id: 4, text: 'Absence request'},{id: 5, text: 'Annual leave request'}]}
+                />
+                {this.state.main}
+            </div>
+
         </div>
 
     );
---
+++ src/patient/patients.jsx
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+import Axios from "axios";
+import Table from "../generic_components/table";
+
+class Patients extends Component{
+    constructor(props) {
+        super(props);
+    }
+
+    state = {
+        patients: []
+    };
+
+    componentDidMount() {
+        this.loadPatients();
+    }
+
+    loadPatients = () => {
+        Axios.get("http://localhost:8080/api/patients/all").then((res)=>{
+            //console.log(res.data);
+            this.setState({patients: res.data});
+        });
+    }
+
+    render() {
+        let headers = [
+            {headId: 'name', text: 'Name'},
+            {headId: 'lastName', text: 'Last name'},
+            {headId: 'healthInsuranceId', text: 'Insurance id'}
+        ];
+
+        let formatted = this.state.patients.map(patient => {
+            let rl = {};
+            for (let header of headers) {
+                rl[header.headId] = patient[header.headId]
+            }
+            rl['rowId'] = patient.id;
+
+            return rl;
+        });
+
+        return (
+            <div className="col">
+                <Table
+                    headers={headers}
+                    rows={formatted}
+                    emptyListMsg={"Cannot render patients"}
+                />
+            </div>
+
+        );
+    }
+
+}
+export default Patients;
