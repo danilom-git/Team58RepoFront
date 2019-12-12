4cb83aa5d32b4e253f88d6bd7708667ad031c05f
djordje <Zoomb1xon>
2019-12-12 21:54:30 +0100

    search patients

--- src/patient/patients.jsx
+++ src/patient/patients.jsx
@@ -8,7 +8,10 @@ class Patients extends Component{
     }
 
     state = {
-        patients: []
+        patients: [],
+        name:"",
+        lastName:"",
+        healthInsuranceId:""
     };
 
     componentDidMount() {
@@ -17,6 +20,26 @@ class Patients extends Component{
 
     loadPatients = () => {
         Axios.get("http://localhost:8080/api/patients/all").then((res)=>{
+            //console.log(res.data);
+            this.setState({patients: res.data});
+        });
+    };
+
+    changeName = (e) => {
+        this.setState({name:e.target.value});
+    }
+
+    changeLastName = (e) => {
+        this.setState({lastName:e.target.value});
+    }
+
+    changeHealthInsuranceId = (e) => {
+        this.setState({healthInsuranceId:e.target.value});
+    }
+
+    handleSearch = (e) => {
+        e.preventDefault();
+        Axios.get("http://localhost:8080/api/patients/name:"+this.state.name+",lastName:"+this.state.lastName+",healthInsuranceId:"+this.state.healthInsuranceId).then((res)=>{
             //console.log(res.data);
             this.setState({patients: res.data});
         });
@@ -41,12 +64,32 @@ class Patients extends Component{
 
         return (
             <div className="col">
-                <Table
-                    headers={headers}
-                    rows={formatted}
-                    emptyListMsg={"Cannot render patients"}
-                    onRowClick={this.props.changeToPatient}
-                />
+                <div className="row">
+                    <div className="col">
+                        <label>Name: </label><input type="text" onChange={this.changeName} />
+                    </div>
+                    <div className="col">
+                        <label>Last name: </label><input type="text" onChange={this.changeLastName}/>
+                    </div>
+                    <div className="col">
+                        <label>Health insurance id: </label><input type="text" onChange={this.changeHealthInsuranceId}/>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-sm-2">
+                        <button onClick={this.handleSearch} className="btn btn-primary mt-2 mb-2">Search</button>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col">
+                        <Table
+                            headers={headers}
+                            rows={formatted}
+                            emptyListMsg={"Cannot render patients"}
+                            onRowClick={this.props.changeToPatient}
+                        />
+                    </div>
+                </div>
             </div>
         );
     }
