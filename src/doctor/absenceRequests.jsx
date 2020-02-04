0e6c629ce22cef685c219c099e81cb7f4d08f416
djordje <Zoomb1xon>
2020-02-04 16:22:27 +0100

    form for denial and request

--- src/doctor/absenceRequests.jsx
+++ src/doctor/absenceRequests.jsx
@@ -2,14 +2,82 @@ import React, {Component} from "react";
 import Axios from "axios";
 import Modal from 'react-modal';
 
+Modal.setAppElement('#root');
+
+const customStyles = {
+    overlay: {zIndex: 10000}
+};
+
 class AbsenceRequests extends Component{
 
     state = {
         requests: [],
-        clinicId:""
+        clinicId:"",
+        modal: false,
+        modal2: false,
+        responseText:"",
+        reason: "",
+        doctorId:"",
+        requestId:""
     };
 
-    componentDidMount() {
+    showModal = () => {
+        this.setState({modal: true});
+    };
+
+    handleModalCloseRequest = () => {
+        this.setState({modal: false});
+    };
+
+    showModal2 = (e,id,uid) => {
+        this.setState({modal2: true});
+        this.setState(() => ({doctorId: uid}));
+        this.setState(() => ({requestId: id}));
+    };
+
+    handleModalCloseRequest2 = () => {
+        this.setState({modal2: false});
+        this.setState({reason:""});
+    };
+
+    handleModalConfirm = () => {
+        this.setState({modal2: false});
+        if(this.state.reason !== "")
+        {
+            Axios({
+                method: "delete",
+                url: 'http://localhost:8080/api/absenceRequests/request:'+this.state.requestId+"/reason:"+this.state.reason + "/user:"+ this.state.doctorId,
+                headers: {'Authorization': 'Bearer ' + localStorage.getItem('token')}
+            }).then(() => {
+                this.loadRequests();
+            });
+        }
+
+    };
+
+    modalReasonChange = (e) => {
+        e.persist();
+        console.log(e.target.value);
+        this.setState((reason)=>({reason: e.target.value}));
+    };
+
+    allowedRequest = (e,id,uid) => {
+        Axios({
+            method: 'put',
+            url : 'http://localhost:8080/api/absenceRequests/request:'+id+"/userId:"+uid+"/",
+            headers: {'Authorization': 'Bearer ' + localStorage.getItem('token')}
+
+        }).then((res)=>{
+            console.log(res.data);
+            if(res.data === true)
+            {
+                this.setState(()=>({responseText:"Request for absence has been accepted."}));
+                this.showModal();
+            }
+        });
+    };
+
+    loadRequests = () =>{
         Axios({
             method: 'get',
             url: 'http://localhost:8080/api/clinicAdmins/user',
@@ -35,9 +103,12 @@ class AbsenceRequests extends Component{
 
                 }
                 this.setState({requests: requests});
-                console.log(res.data);
             });
         });
+    };
+
+    componentDidMount() {
+        this.loadRequests();
     }
 
     render() {
@@ -48,8 +119,8 @@ class AbsenceRequests extends Component{
                 <td>{request.startDate}</td>
                 <td>{request.endDate}</td>
                 <td>{request.type}</td>
-                <td><button className="btn btn-success">Allow</button></td>
-                <td><button className="btn btn-danger">Decline</button></td>
+                <td><button onClick={(e) => this.allowedRequest(e,request.id,request.doctorId)} className="btn btn-success">Allow</button></td>
+                <td><button onClick={(e) => this.showModal2(e,request.id,request.doctorId)} className="btn btn-danger">Decline</button></td>
             </tr>
         ));
 
@@ -71,6 +142,62 @@ class AbsenceRequests extends Component{
                 </tbody>
 
             </table>
+
+            <Modal
+                className="Modal__Bootstrap modal-dialog"
+                closeTimeoutMS={150}
+                isOpen={this.state.modal}
+                style={customStyles}
+                onRequestClose={this.handleModalCloseRequest}
+            >
+                <div className="modal-content" role="dialog">
+                    <div className="modal-header">
+                        <h4 className="modal-title">Notification</h4>
+                        <button type="button" className="close" onClick={this.handleModalCloseRequest}>
+                            <span aria-hidden="true">&times;</span>
+                            <span className="sr-only">Close</span>
+                        </button>
+                    </div>
+                    <div className="modal-body">
+                        <p>{this.state.responseText}</p>
+                    </div>
+                    <div className="modal-footer">
+                        <button type="button" className="btn btn-secondary"
+                                onClick={this.handleModalCloseRequest}>Close
+                        </button>
+                    </div>
+                </div>
+            </Modal>
+
+            <Modal
+                className="Modal__Bootstrap modal-dialog"
+                closeTimeoutMS={150}
+                isOpen={this.state.modal2}
+                style={customStyles}
+                onRequestClose={this.handleModalCloseRequest2}
+            >
+                <div className="modal-content" role="dialog">
+                    <div className="modal-header">
+                        <h4 className="modal-title">Confirmation</h4>
+                        <button type="button" className="close" onClick={this.handleModalCloseRequest2}>
+                            <span aria-hidden="true">&times;</span>
+                            <span className="sr-only">Close</span>
+                        </button>
+                    </div>
+                    <div className="modal-body">
+                        <input className="col" onChange={this.modalReasonChange} type="text" />
+                    </div>
+                    <div className="modal-footer">
+                        <button type="button" className="btn btn-secondary"
+                                onClick={this.handleModalCloseRequest2}>Close
+                        </button>
+                        <button type="button" className="btn btn-secondary"
+                                onClick={this.handleModalConfirm}>Confirm
+                        </button>
+                    </div>
+                </div>
+            </Modal>
+
         </>);
     }
 
