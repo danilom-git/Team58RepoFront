a9aa5c6ac047e2bbd4fa933ea7a43e2e314e7128
djordje <Zoomb1xon>
2020-02-05 16:54:59 +0100

    search form

--- src/hall/searchHall.jsx
+++ src/hall/searchHall.jsx
@@ -1,15 +1,42 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import Axios from "axios";
+import DatePicker from "../generic_components/datepicker";
 
-class SearchHall extends Component{
+class SearchHall extends Component {
     state = {
+        checkupRequest: {}
     };
 
     componentDidMount() {
-        //console.log(this.props.requestId);
+        Axios({
+            method: 'get',
+            url: 'http://localhost:8080/api/checkupRequests/checkupRequest:' + this.props.requestId,
+            headers: {'Authorization': 'Bearer ' + localStorage.getItem('token')}
+        }).then((res) => {
+            this.setState({checkupRequest: res.data});
+            //console.log(res.data);
+        });
     }
 
     render() {
-        return (<></>);
+        return (<>
+            <form>
+                <div className="row">
+                    <div className="col">
+                        <label >Hall name</label>
+                        <input className="form-control" type="text"/>
+                    </div>
+                    <div className="col">
+                        <label >Hall number</label>
+                        <input className="form-control" type="text"/>
+                    </div>
+                    <div className="col">
+                        <DatePicker className="form-control" />
+                    </div>
+                </div>
+            </form>
+        </>);
     }
-}export default SearchHall;
+}
+
+export default SearchHall;
