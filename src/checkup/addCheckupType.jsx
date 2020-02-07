7f490a83fa4fa87adc76d2eff89454fb7942bd78
djordje <Zoomb1xon>
2020-02-07 20:42:00 +0100

    adding checkup post request

--- src/checkup/addCheckupType.jsx
+++ src/checkup/addCheckupType.jsx
@@ -5,7 +5,10 @@ class AddCheckupType extends Component {
 
     state = {
         types: [],
-        price: 0
+        price: 0,
+        typeId: "",
+        name: "",
+        clinicId: ""
     };
 
     componentDidMount() {
@@ -15,6 +18,7 @@ class AddCheckupType extends Component {
             headers: {'Authorization': 'Bearer ' + localStorage.getItem('token')},
             data: {token: localStorage.getItem('token'), expiresIn: 0, userType: ""}
         }).then((res) => {
+            this.setState({clinicId: res.data.clinicId});
             Axios({
                 method: 'get',
                 url: 'http://localhost:8080/api/checkupTypes/allFalse/clinic:' + res.data.clinicId,
@@ -26,9 +30,47 @@ class AddCheckupType extends Component {
         });
     }
 
+    priceChange = (e) => {
+        this.setState({price:Number(e.target.value)});
+    };
+
+    typeChange = (e) => {
+        this.setState({typeId:e.target.value});
+        for(let t of this.state.types)
+        {
+            if(t.id == e.target.value) {
+                //console.log(t);
+                this.setState({name: t.name});
+                break;
+            }
+        }
+    };
+
+    handleSubmit = (e) =>{
+        console.log(this.state);
+        if(this.state.typeId !== "" && this.state.price !== 0)
+        {
+            let data = {
+                price: this.state.price,
+                name: this.state.name,
+                clinicId: this.state.clinicId,
+                id: this.state.typeId
+            };
+
+            Axios({
+                method: 'post',
+                url: 'http://localhost:8080/api/clinicCheckupTypes/clinic:' + this.state.clinicId,
+                headers: {'Authorization': 'Bearer ' + localStorage.getItem('token')},
+                data: data
+            }).then((res)=>{
+                console.log(res.data);
+            });
+        }
+    };
+
     render() {
         let types = this.state.types.map(type => (
-            <option value={type.name} key={type.id}>{type.name}</option>
+            <option value={type.id} key={type.id}>{type.name}</option>
         ));
 
         return (<>
@@ -39,24 +81,24 @@ class AddCheckupType extends Component {
             </div>
             <div className="row">
                 <div className="col-sm-4">
-                    <select className="form-control">
-                        <option value=""></option>
+                    <select onChange={this.typeChange} className="form-control">
+                        <option value="" />
                         {types}</select>
                 </div>
             </div>
             <div className="row">
-                <div className="col-sm-4">
+                <div className="col-sm-4 mt-2">
                     <label>Enter price:</label>
                 </div>
             </div>
             <div className="row">
                 <div className="col-sm-4">
-                    <input className="form-control" type="text"></input>
+                    <input onChange={this.priceChange} className="form-control" type="text" />
                 </div>
             </div>
             <div className="row">
                 <div className="col-sm-4">
-                    <button className="btn btn-primary mt-2">Add</button>
+                    <button onClick={this.handleSubmit} className="btn btn-primary mt-2">Add</button>
                 </div>
             </div>
         </>);
