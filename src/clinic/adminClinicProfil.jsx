c140bb829e8a7fe36be822fba0671e912eb451b7
djordje <Zoomb1xon>
2020-02-07 10:44:12 +0100

    clinic profile changed

--- src/clinic/adminClinicProfil.jsx
+++ src/clinic/adminClinicProfil.jsx
@@ -1,62 +1,96 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import Sidebar from "../generic_components/sidebar";
 import AbsenceRequests from "../doctor/absenceRequests";
 import CheckupRequests from "../checkup/checkupRequests";
 import SearchHall from "../hall/searchHall";
-import { YMaps, Map ,GeoObject} from 'react-yandex-maps';
+import {YMaps, Map, GeoObject} from 'react-yandex-maps';
+import OneClickForm from "../checkup/oneClickForm";
+import HallAddForm from "../hall/hallAddForm";
+import DoctorAddForm from "../doctor/doctorAddForm";
+
 
 class AdminClinicProfil extends Component {
 
-  state = { main: <div>
-          <div id="map"></div>
-          <YMaps>
-              <div>
-                  My awesome application with maps!
-                  <Map state={{ center: [45.75, 25.57], zoom: 9 }}>
-                      <GeoObject
-                          geometry={{
-                              type: 'Point',
-                              coordinates: [45.75, 25.57],
-                          }}
+    state = {
+        main: <div>
+            <div id="map"></div>
+            <YMaps>
+                <div>
+                    My awesome application with maps!
+                    <Map state={{center: [45.75, 25.57], zoom: 9}}>
+                        <GeoObject
+                            geometry={{
+                                type: 'Point',
+                                coordinates: [45.75, 25.57],
+                            }}
 
-                          properties={{
-                              iconContent: 'Lokacija',
-                              hintContent: 'Lokacija',
-                          }}
+                            properties={{
+                                iconContent: 'Lokacija',
+                                hintContent: 'Lokacija',
+                            }}
 
-                      />
-                  </Map>
+                        />
+                    </Map>
 
-              </div>
-          </YMaps>
-      </div> };
-
-  changeToSearchHall = (requestId) => {
-      this.setState({main: <SearchHall changeToRequests={this.changeToCheckupRequests} requestId={requestId} />});
-  };
-
-  changeToRequests = () => {
-    this.setState({main: <AbsenceRequests />});
-  };
-
-  changeToCheckupRequests = () => {
-      this.setState({main: <CheckupRequests changeToSearchHall={this.changeToSearchHall} />});
-  };
-
-  render() {
-
-    return (
-        <div className="container-fluid pt-2">
-          <div className="row">
-
-            <Sidebar links={[{id:1,text:"User profile"},{id:2,text:"Clinic profile",onClick:this.props.changeToClinic},{id:3,text:"Clinic report"},{id:4,text:"Absence requests",onClick:this.changeToRequests},,{id:5,text:"Checkup requests",onClick:this.changeToCheckupRequests}]}
-            />
-
-            <div className="col">{this.state.main}</div>
-          </div>
+                </div>
+            </YMaps>
         </div>
-    );
-  }
+    };
+
+    changeToAddDoctor = () => {
+        this.setState({main: <DoctorAddForm/>});
+    };
+
+    changeToAddHall = () => {
+        this.setState({main: <HallAddForm/>});
+    };
+
+    changeToOneClickForm = () => {
+        this.setState({main: <OneClickForm/>});
+    };
+
+    changeToSearchHall = (requestId) => {
+        this.setState({main: <SearchHall changeToRequests={this.changeToCheckupRequests} requestId={requestId}/>});
+    };
+
+    changeToRequests = () => {
+        this.setState({main: <AbsenceRequests/>});
+    };
+
+    changeToCheckupRequests = () => {
+        this.setState({main: <CheckupRequests changeToSearchHall={this.changeToSearchHall}/>});
+    };
+
+    render() {
+
+        return (
+            <div className="container-fluid pt-2">
+                <div className="row">
+
+                    <Sidebar links={[{id: 1, text: "User profile"}, {
+                        id: 2,
+                        text: "Clinic profile",
+                        onClick: this.props.changeToClinic
+                    }, {id: 3, text: "Clinic report"}, {
+                        id: 4,
+                        text: "Absence requests",
+                        onClick: this.changeToRequests
+                    }, {id: 5, text: "Checkup requests", onClick: this.changeToCheckupRequests}, {
+                        id: 6,
+                        text: "Add doctor",
+                        onClick: this.changeToAddDoctor
+                    }, , {id: 7, text: "Add hall", onClick: this.changeToAddHall}, {
+                        id: 8,
+                        text: "Add checkup",
+                        onClick: this.changeToOneClickForm
+                    }]}
+                    />
+
+                    <div className="col">{this.state.main}</div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default AdminClinicProfil;
--- src/clinic/clinicProfil.jsx
+++ src/clinic/clinicProfil.jsx
@@ -1,32 +1,22 @@
 import React, {Component} from "react";
 import Doctors from "../doctor/doctors";
-import DoctorAddForm from "../doctor/doctorAddForm";
 import Halls from "../hall/halls";
-import HallAddForm from "../hall/hallAddForm";
 import ShowDoctor from "../doctor/showDoctor";
 import Sidebar from "../generic_components/sidebar";
 import ShowHall from "../hall/showHall";
-import OneClickForm from "../checkup/oneClickForm";
 import OneClicks from "../checkup/OneClicks";
 
 class ClinicProfil extends Component {
     state = {main: <div></div>};
 
     showDoctors = () => {
-        this.setState({main: <Doctors admin={this.props.admin} showDoctor={this.changeToShowDoctor}/>});
+        this.setState({main: <Doctors showDoctor={this.changeToShowDoctor}/>});
     };
 
     showHalls = () => {
-        this.setState({main: <Halls admin={this.props.admin} showHall={this.changeToShowHall}/>});
+        this.setState({main: <Halls showHall={this.changeToShowHall}/>});
     };
 
-    changeToAddDoctor = () => {
-        this.setState({main: <DoctorAddForm admin={this.props.admin}/>});
-    };
-
-    changeToAddHall = () => {
-        this.setState({main: <HallAddForm admin={this.props.admin}/>});
-    };
 
     changeToShowDoctor = (e, doctor) => {
         this.setState({main: <ShowDoctor doctor={doctor}/>});
@@ -34,10 +24,6 @@ class ClinicProfil extends Component {
 
     changeToShowHall = (e, hall) => {
         this.setState({main: <ShowHall hall={hall}/>});
-    };
-
-    changeToOneClickForm = () => {
-        this.setState({main: <OneClickForm/>});
     };
 
     changeToOneClick = () => {
@@ -52,17 +38,13 @@ class ClinicProfil extends Component {
                     <Sidebar
                         links={[{id: 1, text: 'Doctors', onClick: this.showDoctors}, {
                             id: 2,
-                            text: 'Add doctor',
-                            onClick: this.changeToAddDoctor
-                        }, {id: 3, text: 'Halls', onClick: this.showHalls}, {
-                            id: 4,
-                            text: 'Add hall',
-                            onClick: this.changeToAddHall
-                        }, {id: 5, text: 'Add checkups', onClick: this.changeToOneClickForm}, {
-                            id: 6,
+                            text: 'Halls',
+                            onClick: this.showHalls
+                        }, {
+                            id: 3,
                             text: 'Checkups',
                             onClick: this.changeToOneClick
-                        }, {id: 7, text: 'Checkup types'}]}/>
+                        }, {id: 4, text: 'Checkup types'}]}/>
 
                     <div className="col">{this.state.main}</div>
                 </div>
