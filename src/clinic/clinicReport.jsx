0be24731f15d785de5ba3acede2e34c3f001331f
djordje <Zoomb1xon>
2020-02-09 16:56:40 +0100

    added filter month,year

--- src/clinic/clinicReport.jsx
+++ src/clinic/clinicReport.jsx
@@ -23,7 +23,7 @@ class ClinicReport extends Component {
             animationEnabled: true,
             theme: "light2",
             title:{
-                text: "Clinic checkups"
+                text: "Clinic checkups chart"
             },
             axisX:{
                 valueFormatString: "DD MMM",
@@ -33,7 +33,7 @@ class ClinicReport extends Component {
                 }
             },
             axisY: {
-                title: "Clinic checkups by dy",
+                title: "Checkups chart",
                 includeZero: false,
                 valueFormatString: "##0",
                 crosshair: {
@@ -55,6 +55,142 @@ class ClinicReport extends Component {
       />);
     };
 
+    filterYear = () => {
+        let checkups = this.state.clinicReport.checkups;
+        let dates = [];
+        for(let c of checkups)
+        {
+            //console.log("C:",c);
+            let date = new Date(c.startDate);
+            let uslov = false;
+            for(let d of dates)// da li ima vec sa tim dateom
+            {
+                //console.log("D:",c);
+                d = new Date(d.x);
+                if((date.getFullYear() === d.getFullYear()))
+                {
+                    //console.log("NASAO ISTI DODAT VEC:",d);
+                    uslov = true;
+                    break;
+                }
+            }
+
+            if(!uslov)// ako nema prebroj
+            {
+                //console.log("BROJI:");
+                let br = 0;
+                for(let cc of checkups)
+                {
+                    cc = new Date(cc.startDate);
+                    //console.log("CC",cc);
+                    if((date.getFullYear() === cc.getFullYear()))
+                    {
+                        //console.log("NASAO ISTI:");
+                        br = br + 1;
+                    }
+                }
+                //console.log("BR: ",br);
+                dates.push({x: date,y: br});
+            }
+
+
+        }
+        this.setState(() => ({dataPoints:dates}));
+        this.setState({rend:true});
+    };
+
+    filterMonth = () => {
+        let checkups = this.state.clinicReport.checkups;
+        let dates = [];
+        for(let c of checkups)
+        {
+            //console.log("C:",c);
+            let date = new Date(c.startDate);
+            let uslov = false;
+            for(let d of dates)// da li ima vec sa tim dateom
+            {
+                //console.log("D:",c);
+                d = new Date(d.x);
+                if((date.getFullYear() === d.getFullYear()) && (date.getMonth() === d.getMonth()))
+                {
+                    //console.log("NASAO ISTI DODAT VEC:",d);
+                    uslov = true;
+                    break;
+                }
+            }
+
+            if(!uslov)// ako nema prebroj
+            {
+                //console.log("BROJI:");
+                let br = 0;
+                for(let cc of checkups)
+                {
+                    cc = new Date(cc.startDate);
+                    //console.log("CC",cc);
+                    if((date.getFullYear() === cc.getFullYear()) && (date.getMonth() === cc.getMonth()))
+                    {
+                        //console.log("NASAO ISTI:");
+                        br = br + 1;
+                    }
+                }
+                //console.log("BR: ",br);
+                dates.push({x: date,y: br});
+            }
+
+
+        }
+        this.setState(() => ({dataPoints:dates}));
+        this.setState({rend:true});
+    };
+
+    filterDay = (e,checkups) => {
+
+        if(!checkups)
+            checkups = this.state.clinicReport.checkups;
+
+        let dates = [];
+        for(let c of checkups)
+        {
+            //console.log("C:",c);
+            let date = new Date(c.startDate);
+            let uslov = false;
+            for(let d of dates)// da li ima vec sa tim dateom
+            {
+                //console.log("D:",c);
+                d = new Date(d.x);
+                if((date.getFullYear() === d.getFullYear()) && (date.getMonth() === d.getMonth()) && (date.getDay() === d.getDay()))
+                {
+                    //console.log("NASAO ISTI DODAT VEC:",d);
+                    uslov = true;
+                    break;
+                }
+            }
+
+            if(!uslov)// ako nema prebroj
+            {
+                //console.log("BROJI:");
+                let br = 0;
+                for(let cc of checkups)
+                {
+                    cc = new Date(cc.startDate);
+                    //console.log("CC",cc);
+                    if((date.getFullYear() === cc.getFullYear()) && (date.getMonth() === cc.getMonth()) && (date.getDay() === cc.getDay()))
+                    {
+                        //console.log("NASAO ISTI:");
+                        br = br + 1;
+                    }
+                }
+                //console.log("BR: ",br);
+                dates.push({x: date,y: br});
+            }
+
+
+        }
+        this.setState(() => ({dataPoints:dates}));
+        this.setState({rend:true});
+    };
+
+
     componentDidMount() {
         Axios({
             method: 'post',
@@ -71,47 +207,8 @@ class ClinicReport extends Component {
                 res.data.clinicRating = res.data.clinicRating.toFixed(2);
                 this.setState({clinicReport:res.data});
                 this.setState({doctors:res.data.doctors});
-                let dates = [];
-                for(let c of res.data.checkups)
-                {
-                    console.log("C:",c);
-                    let date = new Date(c.startDate);
-                    let uslov = false;
-                    for(let d of dates)// da li ima vec sa tim dateom
-                    {
-                        console.log("D:",c);
-                        d = new Date(d.x);
-                        if((date.getFullYear() === d.getFullYear()) && (date.getMonth() === d.getMonth()) && (date.getDay() === d.getDay()))
-                        {
-                            console.log("NASAO ISTI DODAT VEC:",d);
-                            uslov = true;
-                            break;
-                        }
-                    }
-
-                    if(!uslov)// ako nema prebroj
-                    {
-                        console.log("BROJI:");
-                        let br = 0;
-                        for(let cc of res.data.checkups)
-                        {
-                            cc = new Date(cc.startDate);
-                            console.log("CC",cc);
-                            if((date.getFullYear() === cc.getFullYear()) && (date.getMonth() === cc.getMonth()) && (date.getDay() === cc.getDay()))
-                            {
-                                console.log("NASAO ISTI:");
-                                br = br + 1;
-                            }
-                        }
-                        console.log("BR: ",br);
-                        dates.push({x: date,y: br});
-                    }
-
-
-                }
-                this.setState(() => ({dataPoints:dates}));
-                this.setState({rend:true});
-                //console.log(res.data);
+                this.filterDay("",res.data.checkups);
+                console.log(res.data);
             });
         });
     }
@@ -124,14 +221,18 @@ class ClinicReport extends Component {
             </tr>
         ));
 
-        console.log(this.state);
+        //console.log(this.state);
         return (<>
             <div className="row">
                 <div className="col">
                     <h3 >Clinic report:</h3>
                     <div className="row">
                         <div className="col">
-                            Clinic rating: {this.state.clinicReport.clinicRating}
+                            <div className="row">
+                                <div className="col">
+                                    Clinic rating: {this.state.clinicReport.clinicRating}
+                                </div>
+                            </div>
                         </div>
                     </div>
                     <div className="row">
@@ -151,7 +252,19 @@ class ClinicReport extends Component {
                     </div>
                 </div>
                 <div className="col">
+                    <div className="row">
+                        <div className="col">
+                            Filter by:
+                        </div>
+                    </div>
+                    <div className="row">
+                        <button className="btn btn-primary mr-2" onClick={this.filterDay}>Day</button>
+                        <button className="btn btn-primary mr-2" onClick={this.filterMonth}>Month</button>
+                        <button className="btn btn-primary" onClick={this.filterYear}>Year</button>
+                    </div>
+                    <div className="row">
                     {this.state.rend && this.chart()}
+                    </div>
                 </div>
             </div>
 
